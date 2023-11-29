3ebc0053ac05adbcd608fe232c1a2de39ae44b0e
Aeron7 Inc <hello@aeron7.com>
2023-11-29 22:44:30 +0530

    2.8

--- nsepythonserver/rahu.py
+++ nsepythonserver/rahu.py
@@ -927,3 +927,9 @@ def nse_expirydetails_by_symbol(symbol,meta ="Futures",i=0):
     currentExpiry = datetime.datetime.strptime(currentExpiry,'%d-%b-%Y').date()    
     dte = (currentExpiry - datetime.datetime.now().date()).days
     return currentExpiry,dte
+
+def security_wise_archive(from_date, to_date, symbol, series="ALL"):   
+    base_url = "https://www.nseindia.com/api/historical/securityArchives"
+    url = f"{base_url}?from={from_date}&to={to_date}&symbol={symbol.upper()}&dataType=priceVolumeDeliverable&series={series.upper()}"
+    payload = nsefetch(url)
+    return pd.DataFrame(payload['data'])
